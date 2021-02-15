cc5b1d28648a3c4c20af25e05666db0d6979cfbc
Fabricio Carvalho <fcarvalho1999@gmail.com>
2021-02-14 23:18:18 -0300

    New component added
    The LoginForm.jsx is responsible for the login system.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {ChatEngine} from 'react-chat-engine';
-import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
 
 const App = () => {
+
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="8da16ccf-016f-4ae0-8ac4-0d69f6a6969b"
-      userName="Hostjs"
-      userSecret="koalautra1230"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   )
